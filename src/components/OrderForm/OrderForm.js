fd9ba36e0308afa0873cce8d472e5c3250af3953
James Carlson <carlson.james@gmail.com>
2018-03-16 17:36:34 -0600

    initial order form and starting server

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from '../Nav/Nav';
+import Menu from '../Menu/Menu';
 export default class OrderForm extends React.Component {
     constructor(props) {
         super(props)
@@ -9,18 +10,29 @@ export default class OrderForm extends React.Component {
         }
     }
 
+    componentDidMount(){
+        axios.get('api/getmenu').then(res => 
+        this.setState({offerings: res.data}))
+    }
+
     render() {
         return (
             <div>
                 <Nav />
                 <section className="orderform">
-                <form>
-                <div className="orderform" id="title">Let's get your order!</div>
-                <div className="prompt">Name: <input label="name" placeholder="enter your name here" /></div>
-                <div>What do you want to order today?</div>
-                <div>When would you like to pick it up?</div>
-                <div>How would you like to pay?</div>
-                </form>
+                    <form>
+                        <div id="title">Tell us what you want!</div>
+                        <div className="form-body">
+                            <div className="form-row">
+                                <div className="prompt"><b>Name</b></div>
+                                <div className="prompt answer"><input label="name" placeholder="type your name here" /></div>
+                            </div>
+                            <div className="prompt"><div>Select from below:</div><Menu offerings={this.state.offerings} /> {/* menu selections should go to state */}
+                            </div>
+                            <div className="prompt">When would you like to pick it up? <input label="name" placeholder="enter your name here" /></div>
+                            <div className="prompt">How would you like to pay? <input label="name" placeholder="enter your name here" /></div>
+                        </div>
+                    </form>
                 </section>
             </div>
         )
